d35fdf76ebc008efca21e7850fec6edaf869c103
Paul Buonopane <paul+git@earth2me.com>
2016-01-21 03:15:13 -0500

    Deployment script for CowCraft

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+set -e
+
+jar=MCPerf.jar
+server=mc-0.internal.cowcraft.net
+
+cd jar || exit $?
+
+if [ $# -eq 0 ]; then
+	set -- dev kitpvp || exit $?
+fi
+
+x=0
+for i in dev kitpvp; do
+	echo "Deploying to $i" >&2
+	case "$i" in
+		common)
+			dest="/mc/$i/plugins/$jar"
+			;;
+
+		*)
+			dest="/mc/servers/$i/plugins/$jar"
+			;;
+	esac
+	
+	rsync --rsync-path='sudo rsync' "$jar" "$server:$dest" || x=$?
+done
+exit $x
